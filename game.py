d727e74b506729efa3072b3aec04e04dceeb54bb
wpp5 <wpp5@georgetown.edu>
2021-02-08 11:50:17 -0500

    Update game.py

--- game.py
+++ game.py
@@ -5,19 +5,25 @@ delay = 1.5
 ComputerOptions = ["rock","paper","scissors"]
 #Collecting users name
 UserName = input("What is your name?")
+#This delay is to simulate the computer thinking
 time.sleep(delay)
 print("Welcome", UserName, "to my Rock-Paper-Scissors game")
 time.sleep(delay)
 x = "yes"
+#This while loop will keep the programing running for the user to keep playing
 while x == "yes":
+    #This variables takes the user input
     UserChoice = input("Please choose from the following. Rock, paper, scissors").lower()
+    #This statement will catch any user inpits outside of rock paper scissors
     if UserChoice in ComputerOptions:
+        #This will generate a random computer choice
         ComputerChoice = random.choice(ComputerOptions)
         time.sleep(delay)
         print("You chose:",UserChoice)
         time.sleep(delay)
         print("Computer chose:",ComputerChoice)
         time.sleep(delay)
+        #This series of if statements will tell the user whether user won or loss
         if UserChoice == ComputerChoice:
             print("It's tie!")
         elif UserChoice == "paper" and ComputerChoice == "rock":
